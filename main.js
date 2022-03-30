d0be751ff8c6cc74f7a51379f5c4811917158f39
Ivan Dario Niello <ivaniello@hotmail.com>
2022-03-30 16:11:15 -0300

    primerdesafio

---
+++ main.js
@@ -0,0 +1,7 @@
+alert ("Aprende a contar de 2 en 2");
+
+let numeroInicial = parseInt(prompt("escribe un numero para empezar"));
+
+for (numeroInicial > 0; numeroInicial <= 50; numeroInicial += 2){
+    console.log(numeroInicial);
+}
